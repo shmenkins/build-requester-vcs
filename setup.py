a623f884ea175089869ecffd897abf018da468c5
Renat <rzhilkibaev@gmail.com>
2017-09-06 21:12:54 -0700

    rename

--- setup.py
+++ setup.py
@@ -7,16 +7,17 @@ from os.path import basename, splitext
 
 from setuptools import find_packages, setup
 
+name = "build-requester-vcs"
 
 setup(
-    name="Webhook",
+    name=name,
     version="0.1.0",
     license="MIT",
     description="Build Requester Component.",
     long_description="Build Requester Component.",
     author="Renat Zhilkibaev",
     author_email="rzhilkibaev@gmail.com",
-    url="https://github.com/shmenkins/build-requester-vcs",
+    url="https://github.com/shmenkins/" + name,
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
